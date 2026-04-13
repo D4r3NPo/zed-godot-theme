5bf24f48747efc89728a393773371457f2976de1
D4r3N <enzo.suares@icloud.com>
2026-04-13 18:30:21 +0200

    Update test.gd

--- tests/test.gd
+++ tests/test.gd
@@ -6,13 +6,9 @@ class_name MyClass extends Node
 
 signal test_signal
 
-var a = 5
-var s = "Hello Mom!"
-var arr = [1, 2, 3]
+var s := "Hello Mom!"
+var arr: Array[int] = [1, 2, 3]
 var dict = {"key": "value", 2: 3}
-var other_dict = {key = "value", other_key = 2}
-var typed_var: int
-var inferred_type := "String"
 const ANSWER = 42
 const THE_NAME = "Charly"
 enum {UNIT_NEUTRAL, UNIT_ENEMY, UNIT_ALLY}
@@ -30,7 +26,6 @@ var property: int:
 func some_function(param1, param2, param3 = 123):
 	print("Hello Dad!")
 	const local_const = PI
-	var local_var: int = 3
 	if param1 < local_const: print("Ok")
 	elif param2 > 5: print(param2)
 	else: print("Fail!")
@@ -39,16 +34,11 @@ func some_function(param1, param2, param3 = 123):
 	match param3:
 		3: print("param3 is 3!")
 		_: print("param3 is not 3!")
-	something(param1, param2)
-	return local_var
+	something()
+	self.something()
 
-# not used parameters
-func something(p1, p2):
+func something():
 	super.set_process(true)
-
-func other_something(p1, p2):
-	# error
-	super.something(p1, p2)
 
 class InnerClass:
 	var a = 10
